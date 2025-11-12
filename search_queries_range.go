ee56ab4489b0b17ce4ae7e470429bc1a70ce67ad
sharkchili <1946955309@qq.com>
2025-11-13 00:24:38 +0800

    refactor(search):重构范围查询以支持lte和gte参数
    - 移除了includeLower和includeUpper字段
    - 添加了lte和gte字段用于精确范围匹配
    - 修改Gte方法设置gte字段而非from和includeLower- 修改Lte方法设置lte字段而非to和includeUpper- 更新序列化逻辑以使用lte和gte参数
    - 移除了include_lower和include_upper的序列化代码

--- search_queries_range.go
+++ search_queries_range.go
@@ -9,21 +9,25 @@ package elastic
 // For details, see
 // https://www.elastic.co/guide/en/elasticsearch/reference/7.0/query-dsl-range-query.html
 type RangeQuery struct {
-	name         string
-	from         interface{}
-	to           interface{}
-	timeZone     string
-	includeLower bool
-	includeUpper bool
-	boost        *float64
-	queryName    string
-	format       string
-	relation     string
+	name     string
+	from     interface{}
+	to       interface{}
+	lte      interface{}
+	gte      interface{}
+	timeZone string
+	//includeLower bool
+	//includeUpper bool
+	boost     *float64
+	queryName string
+	format    string
+	relation  string
 }
 
 // NewRangeQuery creates and initializes a new RangeQuery.
 func NewRangeQuery(name string) *RangeQuery {
-	return &RangeQuery{name: name, includeLower: true, includeUpper: true}
+	return &RangeQuery{name: name} //includeLower: true,
+	//includeUpper: true,
+
 }
 
 // From indicates the from part of the RangeQuery.
@@ -37,15 +41,17 @@ func (q *RangeQuery) From(from interface{}) *RangeQuery {
 // Use nil to indicate an unbounded from part.
 func (q *RangeQuery) Gt(from interface{}) *RangeQuery {
 	q.from = from
-	q.includeLower = false
+	//q.includeLower = false
 	return q
 }
 
 // Gte indicates a greater-than-or-equal value for the from part.
 // Use nil to indicate an unbounded from part.
 func (q *RangeQuery) Gte(from interface{}) *RangeQuery {
-	q.from = from
-	q.includeLower = true
+	//q.from = from
+	//q.includeLower = true
+	q.from = nil
+	q.gte = from
 	return q
 }
 
@@ -60,29 +66,31 @@ func (q *RangeQuery) To(to interface{}) *RangeQuery {
 // Use nil to indicate an unbounded to part.
 func (q *RangeQuery) Lt(to interface{}) *RangeQuery {
 	q.to = to
-	q.includeUpper = false
+	//q.includeUpper = false
 	return q
 }
 
 // Lte indicates a less-than-or-equal value for the to part.
 // Use nil to indicate an unbounded to part.
 func (q *RangeQuery) Lte(to interface{}) *RangeQuery {
-	q.to = to
-	q.includeUpper = true
+	//q.to = to
+	//q.includeUpper = true
+	q.to = nil
+	q.lte = to
 	return q
 }
 
 // IncludeLower indicates whether the lower bound should be included or not.
 // Defaults to true.
 func (q *RangeQuery) IncludeLower(includeLower bool) *RangeQuery {
-	q.includeLower = includeLower
+	//q.includeLower = includeLower
 	return q
 }
 
 // IncludeUpper indicates whether the upper bound should be included or not.
 // Defaults to true.
 func (q *RangeQuery) IncludeUpper(includeUpper bool) *RangeQuery {
-	q.includeUpper = includeUpper
+	//q.includeUpper = includeUpper
 	return q
 }
 
@@ -130,8 +138,10 @@ func (q *RangeQuery) Source() (interface{}, error) {
 	params := make(map[string]interface{})
 	rangeQ[q.name] = params
 
-	params["from"] = q.from
-	params["to"] = q.to
+	//params["from"] = q.from
+	//params["to"] = q.to
+	params["lte"] = q.lte
+	params["gte"] = q.gte
 	if q.timeZone != "" {
 		params["time_zone"] = q.timeZone
 	}
@@ -144,8 +154,8 @@ func (q *RangeQuery) Source() (interface{}, error) {
 	if q.boost != nil {
 		params["boost"] = *q.boost
 	}
-	params["include_lower"] = q.includeLower
-	params["include_upper"] = q.includeUpper
+	//params["include_lower"] = q.includeLower
+	//params["include_upper"] = q.includeUpper
 
 	if q.queryName != "" {
 		rangeQ["_name"] = q.queryName
